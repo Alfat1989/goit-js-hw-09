cef4be95569f46221a1e8e3fb71dc0464d9c9299
Alfat1989 <gilmalf89@gmail.com>
2022-04-09 22:01:55 +0300

    добавляет условия в функцию jnClose

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,11 +5,11 @@ const inputEl = document.querySelector('#datetime-picker')
 const btnStart = document.querySelector('button[data-start]')
 const timerForm = document.querySelector('.timer')
 
-btnStart.addEventListener('click', onBtnStart)
+
+btnStart.setAttribute('disabled', true)
 
 function onBtnStart() {
-    const timeSale=inputEl.value
-    console.log(timeSale)
+    
 }
 
 const data = new Date()
@@ -22,11 +22,29 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    const inputChooseDate=selectedDates[0].getTime()
+    console.log(inputChooseDate)
+    const nowDate=options.defaultDate.getTime()
+    if (inputChooseDate<nowDate) {
+      alert("Please choose a date in the future")
+      return
+    }
+    const difference = inputChooseDate - nowDate
+    btnStart.removeAttribute('disabled')
+    btnStart.addEventListener('click', onBtnStart)
+    console.log(difference)
   },
 };
 
 flatpickr(inputEl, options)
-console.log(options.selectedDates)
+
+// function getDifference(){
+//   const inputCHoseDate = this.selectedDates[0].bind(options).getTime()
+//   console.log(inputCHoseDate)
+//   const nowDate = this.options.defaultDate.getTime()
+//   const difference = inputCHoseDate - nowDate
+//   console.log(difference)
+// }
 
 
 
